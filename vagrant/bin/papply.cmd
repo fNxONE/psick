4258b94b6237b134232b59fc5d0fe8fe05a9233b
Alessandro Franceschi <al@example42.com>
2017-10-11 23:06:13 +0200

    Added links for special host environment

--- vagrant/bin/papply.cmd
+++
@@ -1,6 +0,0 @@
-set base_dir="C:\vagrant_puppet"
-set manifest="C:\vagrant_puppet\manifests\site.pp"
-
-puppet apply -t --environmentpath "C:\" --environment "vagrant_puppet" --detailed-exitcodes %manifest%
-
-
